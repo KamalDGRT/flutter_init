ea63ab80397074ce970ec3bd0a30e025af99babf
Kamal Sharma <kamaldgrt1@gmail.com>
2020-08-11 23:50:22 +0530

    Rows and Columns

--- lib/main.dart
+++ lib/main.dart
@@ -18,35 +18,35 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Awesome App"),
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            // clipBehavior: Clip.antiAlias,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(10),
-                gradient: LinearGradient(colors: [
-                  Colors.pink,
-                  Colors.yellow,
-                ]),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 10,
-                  )
-                ]),
-            child: Text(
-              "I am a Box",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+        body: Container(
+          width: 500,
+          color: Colors.teal,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.redAccent,
+                alignment: Alignment.center,
               ),
-            ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.greenAccent,
+                alignment: Alignment.center,
+              ),
+            ],
           ),
         ));
   }
