1cafe2fde13fefee7b6ea004db3abb8b675b0341
Kamal Sharma <kamaldgrt1@gmail.com>
2020-08-12 02:15:56 +0530

    Drawer and FloatingActionButton Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Awesome App"),
-        ),
-        body: Container(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Center(
+        child: Container(
           height: 100,
+          width: 100,
           color: Colors.teal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.redAccent,
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.greenAccent,
-                alignment: Alignment.center,
-              ),
-            ],
+        ),
+      ),
+      drawer: Drawer(
+          child: ListView(
+        padding: const EdgeInsets.all(0),
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Leo Valdez"),
+            accountEmail: Text("leovaldez@chb.com"),
+            // currentAccountPicture: CircleAvatar(),
           ),
-        ));
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Leo Valdez"),
+            subtitle: Text("The Scrawny Kid"),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("Email"),
+            subtitle: Text("leovaldez@chb.com"),
+            trailing: Icon(Icons.edit),
+          ),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
